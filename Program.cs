c719dace7df54478c5fe1f44894a7b223f7e54c7
Ant198 <ridnovir@yahoo.com>
2024-01-08 13:56:51 +0200

    start project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+
+   public static void Main()
+   {
+        int menuNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(menuNumber);
+   } 
+}
